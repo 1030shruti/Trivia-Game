bc9ba057bef5979e00e427c2209f1de726c2c1b2
CCLC <cclc@lr.k12.nj.us>
2017-08-04 10:27:51 -0400

    Trivia-Game

---
+++ TriviaGame/TriviaGame/Form1.Designer.vb
@@ -0,0 +1,252 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Submit = New System.Windows.Forms.Button()
+        Me.Q1 = New System.Windows.Forms.Button()
+        Me.Q2 = New System.Windows.Forms.Button()
+        Me.Q3 = New System.Windows.Forms.Button()
+        Me.Q4 = New System.Windows.Forms.Button()
+        Me.Q5 = New System.Windows.Forms.Button()
+        Me.Q6 = New System.Windows.Forms.Button()
+        Me.Q7 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(52, 3)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(52, 26)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(52, 49)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(52, 72)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton4.TabIndex = 3
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.RadioButton1)
+        Me.Panel1.Controls.Add(Me.RadioButton4)
+        Me.Panel1.Controls.Add(Me.RadioButton2)
+        Me.Panel1.Controls.Add(Me.RadioButton3)
+        Me.Panel1.Controls.Add(Me.Submit)
+        Me.Panel1.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel1.Location = New System.Drawing.Point(500, 184)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(587, 176)
+        Me.Panel1.TabIndex = 4
+        '
+        'Submit
+        '
+        Me.Submit.Location = New System.Drawing.Point(34, 108)
+        Me.Submit.Name = "Submit"
+        Me.Submit.Size = New System.Drawing.Size(75, 23)
+        Me.Submit.TabIndex = 4
+        Me.Submit.Text = "Submit"
+        Me.Submit.UseVisualStyleBackColor = True
+        '
+        'Q1
+        '
+        Me.Q1.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q1.Location = New System.Drawing.Point(12, 24)
+        Me.Q1.Name = "Q1"
+        Me.Q1.Size = New System.Drawing.Size(148, 23)
+        Me.Q1.TabIndex = 6
+        Me.Q1.Text = "Question 1"
+        Me.Q1.UseVisualStyleBackColor = True
+        '
+        'Q2
+        '
+        Me.Q2.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q2.Location = New System.Drawing.Point(156, 24)
+        Me.Q2.Name = "Q2"
+        Me.Q2.Size = New System.Drawing.Size(158, 23)
+        Me.Q2.TabIndex = 7
+        Me.Q2.Text = "Question 2"
+        Me.Q2.UseVisualStyleBackColor = True
+        '
+        'Q3
+        '
+        Me.Q3.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q3.Location = New System.Drawing.Point(309, 24)
+        Me.Q3.Name = "Q3"
+        Me.Q3.Size = New System.Drawing.Size(155, 23)
+        Me.Q3.TabIndex = 8
+        Me.Q3.Text = "Question 3"
+        Me.Q3.UseVisualStyleBackColor = True
+        '
+        'Q4
+        '
+        Me.Q4.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q4.Location = New System.Drawing.Point(463, 24)
+        Me.Q4.Name = "Q4"
+        Me.Q4.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Q4.Size = New System.Drawing.Size(156, 23)
+        Me.Q4.TabIndex = 9
+        Me.Q4.Text = "Question 4"
+        Me.Q4.UseVisualStyleBackColor = True
+        '
+        'Q5
+        '
+        Me.Q5.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q5.Location = New System.Drawing.Point(615, 24)
+        Me.Q5.Name = "Q5"
+        Me.Q5.Size = New System.Drawing.Size(166, 23)
+        Me.Q5.TabIndex = 10
+        Me.Q5.Text = "Question 5"
+        Me.Q5.UseVisualStyleBackColor = True
+        '
+        'Q6
+        '
+        Me.Q6.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q6.Location = New System.Drawing.Point(779, 24)
+        Me.Q6.Name = "Q6"
+        Me.Q6.Size = New System.Drawing.Size(159, 23)
+        Me.Q6.TabIndex = 11
+        Me.Q6.Text = "Question 6"
+        Me.Q6.UseVisualStyleBackColor = True
+        '
+        'Q7
+        '
+        Me.Q7.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Q7.Location = New System.Drawing.Point(935, 24)
+        Me.Q7.Name = "Q7"
+        Me.Q7.Size = New System.Drawing.Size(152, 23)
+        Me.Q7.TabIndex = 12
+        Me.Q7.Text = "Question 7"
+        Me.Q7.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(97, 111)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 19)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Score"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(512, 141)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(162, 19)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Choose A Question To Start"
+        '
+        'Panel2
+        '
+        Me.Panel2.Font = New System.Drawing.Font("Segoe Print", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Panel2.Location = New System.Drawing.Point(12, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1075, 50)
+        Me.Panel2.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Honeydew
+        Me.ClientSize = New System.Drawing.Size(1099, 561)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Q7)
+        Me.Controls.Add(Me.Q6)
+        Me.Controls.Add(Me.Q5)
+        Me.Controls.Add(Me.Q4)
+        Me.Controls.Add(Me.Q3)
+        Me.Controls.Add(Me.Q2)
+        Me.Controls.Add(Me.Q1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(1115, 600)
+        Me.MinimumSize = New System.Drawing.Size(1115, 600)
+        Me.Name = "Form1"
+        Me.Text = "Trivia Game - Holidays"
+        Me.TransparencyKey = System.Drawing.Color.White
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Submit As Button
+    Friend WithEvents Q1 As Button
+    Friend WithEvents Q2 As Button
+    Friend WithEvents Q3 As Button
+    Friend WithEvents Q4 As Button
+    Friend WithEvents Q5 As Button
+    Friend WithEvents Q6 As Button
+    Friend WithEvents Q7 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel2 As Panel
+End Class
---
+++ TriviaGame/TriviaGame/Form1.vb
@@ -0,0 +1,132 @@
+﻿Public Class Form1
+    Dim x As Integer
+
+    Dim Score As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+
+    Private Sub Submit_Click(sender As Object, e As EventArgs) Handles Submit.Click
+        If RadioButton1.Checked And x = 1 Then
+            MsgBox("Correct")
+            Score = Score + 1
+        ElseIf RadioButton3.Checked And x = 2 Then
+            MsgBox("Correct")
+            Score = Score + 1
+
+        ElseIf RadioButton2.Checked And x = 3 Then
+            MsgBox("Correct")
+            Score = Score + 1
+        ElseIf RadioButton4.Checked And x = 4 Then
+            MsgBox("Correct")
+            Score = Score + 1
+
+        ElseIf RadioButton3.Checked And x = 5 Then
+            MsgBox("Correct")
+            Score = Score + 1
+
+        ElseIf RadioButton1.Checked And x = 6 Then
+            MsgBox("Correct")
+            Score = Score + 1
+
+        ElseIf RadioButton2.Checked And x = 7 Then
+            MsgBox("Correct")
+            Score = Score + 1
+
+        Else MsgBox("Incorrect")
+
+
+
+        End If
+        Label2.Text = "Score: " & Score
+        Panel1.Enabled = False
+
+    End Sub
+
+    Private Sub Q1_Click(sender As Object, e As EventArgs) Handles Q1.Click
+        Panel1.Enabled = True
+        Label1.Text = "How many days are there in a year?"
+        RadioButton1.Text = "365"
+        RadioButton2.Text = "400"
+        RadioButton3.Text = "364"
+        RadioButton4.Text = "360"
+        x = 1
+        Q1.Enabled = False
+
+
+    End Sub
+
+    Private Sub Q2_Click(sender As Object, e As EventArgs) Handles Q2.Click
+        Panel1.Enabled = True
+        Label1.Text = "When is Valentine's Day?"
+        RadioButton1.Text = "Feb. 27th"
+        RadioButton2.Text = "March 12th"
+        RadioButton3.Text = "Feb. 14"
+        RadioButton4.Text = "Jan. 14"
+
+        x = 2
+        Q2.Enabled = False
+
+    End Sub
+
+    Private Sub Q3_Click(sender As Object, e As EventArgs) Handles Q3.Click
+        Panel1.Enabled = True
+        Label1.Text = "WHat Holiday is on the 24th of December?"
+        RadioButton1.Text = "Christmas"
+        RadioButton2.Text = "Christmas Eve"
+        RadioButton3.Text = "Halloween"
+        RadioButton4.Text = "New Year's Eve"
+        x = 3
+        Q3.Enabled = False
+    End Sub
+
+    Private Sub Q4_Click(sender As Object, e As EventArgs) Handles Q4.Click
+        Panel1.Enabled = True
+        Label1.Text = "What happens on October 30th? "
+        RadioButton1.Text = "Halloween"
+        RadioButton2.Text = "First Day of Fall"
+        RadioButton3.Text = "Easter"
+        RadioButton4.Text = "Mischief Night"
+        x = 4
+        Q4.Enabled = False
+    End Sub
+
+    Private Sub Q5_Click(sender As Object, e As EventArgs) Handles Q5.Click
+        Panel1.Enabled = True
+        Label1.Text = "What is the difference between Cyber Monday and Black Friday?"
+        RadioButton1.Text = " Nothing"
+        RadioButton2.Text = " Black Friday is an online sale and Cyber Monday is an in-store sale."
+        RadioButton3.Text = "Black Friday is an in-store sale and cyber monday is an online sale"
+        RadioButton4.Text = "On black friday, the sale is only in the morning; on cyber monday the sale is only in the afternoon."
+        x = 5
+        Q5.Enabled = False
+
+    End Sub
+
+    Private Sub Q6_Click(sender As Object, e As EventArgs) Handles Q6.Click
+        Panel1.Enabled = True
+        Label1.Text = "What day represents that YOU have gone a full circle  around the Sun?"
+        RadioButton1.Text = "Your Birthday"
+        RadioButton2.Text = "New Year's"
+        RadioButton3.Text = "Halloween"
+        RadioButton4.Text = "Christmas"
+        x = 6
+        Q6.Enabled = False
+    End Sub
+
+    Private Sub Q7_Click(sender As Object, e As EventArgs) Handles Q7.Click
+        Panel1.Enabled = True
+        Label1.Text = "How many times do we have to change the time?"
+        RadioButton1.Text = "1"
+        RadioButton2.Text = "2"
+        RadioButton3.Text = "3"
+        RadioButton4.Text = "4"
+        x = 7
+        Q7.Enabled = False
+    End Sub
+
+End Class
+
+
